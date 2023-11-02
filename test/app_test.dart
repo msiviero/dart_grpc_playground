21fe02e4be4f7d8160e5693fe6810de535ca8280
Marco Siviero <m.siviero83@gmail.com>
2023-11-02 18:08:08 +0100

    Added mockito

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,9 +1,13 @@
 import 'package:dart_grpc_playground/api/user_route.dart';
 import 'package:dart_grpc_playground/app.dart';
+import 'package:mockito/annotations.dart';
 import 'package:test/test.dart';
+
+@GenerateNiceMocks([MockSpec<UserRoute>()])
+import 'app_test.mocks.dart';
 
 void main() {
   test('app exists', () {
-    expect(App(UserRoute()), isNotNull);
+    expect(App(MockUserRoute()), isNotNull);
   });
 }
